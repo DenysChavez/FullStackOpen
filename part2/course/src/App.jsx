86a5270220fd0e123cbff09af57aea719bea46a3
Denys Chavez <dvchf26@gmail.com>
2024-06-13 12:03:31 -0400

    Part2 exercise 2.1: Course information step 6

---
+++ part2/course/src/App.jsx
@@ -0,0 +1,46 @@
+import Course from "./components/Course";
+
+// const Total = (props) => {
+//   const total =
+//     props.parts[0].exercises +
+//     props.parts[1].exercises +
+//     props.parts[2].exercises;
+  
+//   return (
+//     <>
+//       <p>Number of exercises {total}</p>
+//     </>
+//   );
+// };
+
+const App = () => {
+const course = {
+  id: 1,
+  name: "Half Stack application development",
+  parts: [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+      id: 1,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+      id: 2,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+      id: 3,
+    },
+  ],
+};
+
+  return (
+    <div>
+      <Course course={course}/>
+    </div>
+  );
+};
+
+export default App;
