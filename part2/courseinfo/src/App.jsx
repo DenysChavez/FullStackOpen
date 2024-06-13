5370392cd9e01da484320e4bfa0d037416bb1213
Denys Chavez <dvchf26@gmail.com>
2024-06-13 12:12:35 -0400

    Part2 exercises 2.2: Course information step7

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,18 +1,5 @@
 import Course from "./components/Course";
 
-// const Total = (props) => {
-//   const total =
-//     props.parts[0].exercises +
-//     props.parts[1].exercises +
-//     props.parts[2].exercises;
-  
-//   return (
-//     <>
-//       <p>Number of exercises {total}</p>
-//     </>
-//   );
-// };
-
 const App = () => {
 const course = {
   id: 1,
---
+++ part2/courseinfo/src/components/Total.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Total = ({ parts }) => {
+    const total = parts[0].exercises + parts[1].exercises + parts[2].exercises;
+
+  return (
+    <>
+      <p>Total of exercises {total}</p>
+    </>
+  );
+};
+
+export default Total
