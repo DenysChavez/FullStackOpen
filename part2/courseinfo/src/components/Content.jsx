04378f47938e550ba3fed0608db4755fb47a59ca
Denys Chavez <dvchf26@gmail.com>
2024-06-13 13:46:28 -0400

    Part2 exercises 2.4: Course information step 9

--- part2/courseinfo/src/components/Content.jsx
+++ part2/courseinfo/src/components/Content.jsx
@@ -2,12 +2,13 @@ import Part from "./Part"
 import Total from "./Total";
 
 const Content = ({ parts }) => {
+
   return (
     <>
-      <Part part={parts[0].name} exercises={parts[0].exercises} />
-      <Part part={parts[1].name} exercises={parts[1].exercises} />
-      <Part part={parts[2].name} exercises={parts[2].exercises} />
-      <Total parts={parts}/>
+      {parts.map(part => 
+        <Part key={part.id} part={part}/>
+      )}
+      <Total parts={parts} />
     </>
   );
 }
