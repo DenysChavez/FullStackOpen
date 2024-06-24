e2f46121c425ee0ddad24f87a061636f8d374146
Denys Chavez <dvchf26@gmail.com>
2024-06-24 14:45:19 -0400

    Part2 e.Adding styles to React app: Inline styles

--- part2/part2-notes-frontend-part2-1/src/App.jsx
+++ part2/part2-notes-frontend-part2-1/src/App.jsx
@@ -3,6 +3,22 @@ import noteService from "./services/notes";
 import Note from './components/Note'
 import Notification from './components/Notification';
 
+const Footer = () => {
+  const footerStyle = {
+    color: "green",
+    fontStyle: "italic",
+    fontSize: 16,
+  };
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>
+        Note app, Department of Computer Science, University of Helsinki 2024
+      </em>
+    </div>
+  );
+};
+
 
 const App = () => {
   const [notes, setNotes] = useState([]);
@@ -82,6 +98,8 @@ const App = () => {
         <input value={newNote} onChange={handleNoteChange} />
         <button type="submit">save</button>
       </form>
+
+      <Footer/>
     </div>
   );
 
