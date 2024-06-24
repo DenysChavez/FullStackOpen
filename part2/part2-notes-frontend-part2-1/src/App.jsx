356507d43131a23a06ea131c2efe2256b8dad8e5
Denys Chavez <dvchf26@gmail.com>
2024-06-23 20:01:14 -0400

    Part2 d.Alterind data in server: Sendind Data to the Server

--- part2/part2-notes-frontend-part2-1/src/App.jsx
+++ part2/part2-notes-frontend-part2-1/src/App.jsx
@@ -26,11 +26,14 @@ useEffect(hook, []);
     const noteObject = {
       content: newNote,
       important: Math.random() < 0.5,
-      id: notes.length + 1,
     }
 
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+    axios
+      .post("http://localhost:3001/notes", noteObject)
+      .then(response => {
+        setNotes(notes.concat(response.data))
+        setNewNote("")
+      })
   }
 
   const handleNoteChange = (event) => {
