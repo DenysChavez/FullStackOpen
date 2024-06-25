4bb34774ac5ccb3c91c3c4a64055028d7ab6dd3c
Denys Chavez <dvchf26@gmail.com>
2024-06-25 17:25:40 -0400

    Part2 e.Adding styles to React app: Exercise 2.19

---
+++ part2/countries/src/components/CountryList.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Country from './Country'
+
+const CountryList = ({ countries, showCountry }) => {
+    
+
+    if (countries.length > 10) {
+        return (
+            <div>
+                Too many matches, specify another filter
+            </div>
+        )
+    }
+
+    if (countries.lenght === 1) {
+        return <Country country={countries[0]} />;
+    }
+
+  return (
+      <div>
+          {countries.map(c => 
+              <p key={c.fifa}>
+                  {c.name.common}
+                  <button onClick={() => showCountry(c.name.common)}>show</button>
+              </p>
+          )}
+      </div>
+  )
+}
+
+export default CountryList;
